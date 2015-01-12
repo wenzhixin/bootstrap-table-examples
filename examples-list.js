3d9b040d444e16cce499b3c4a439544667254946
zhixin <wenzhixin2010@gmail.com>
2015-01-13 00:06:47 +0800

    Add auto create README.

---
+++ examples-list.js
@@ -0,0 +1,22 @@
+var fs = require('fs'),
+    baseUrl = 'http://issues.wenzhixin.net.cn/bootstrap-table/';
+
+fs.readdir('.', function (err, list) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    var markdown = [fs.readFileSync('./README.conf').toString()];
+
+    list.forEach(function (file) {
+        if (/^\d+.html$/.test(file)) {
+            var content = fs.readFileSync(file).toString(),
+                m = content.match(/<title>(.*)<\/title>/);
+
+            if (m) {
+                markdown.push('* ' + '[' + m[1] + '](' + baseUrl + file + ')');
+            }
+        }
+    });
+    fs.writeFileSync('./README.md', markdown.join('\n'));
+});

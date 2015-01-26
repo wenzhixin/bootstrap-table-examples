e13fe3e4f8b79c6440bec4eea575763748f8cc9c
zhixin <wenzhixin2010@gmail.com>
2015-01-26 09:34:35 +0800

    Update index and template

--- methods/template.js
+++ methods/template.js
@@ -13,4 +13,4 @@ content = sprintf(content, {
     title: title
 });
 
-fs.writeFileSync(sprintf('./%s.html', title), content);
+fs.writeFileSync(sprintf('./%s.html', title.replace(/\//, '-')), content);

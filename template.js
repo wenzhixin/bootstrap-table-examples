068499a60c959c063c81fa9065bf3eacb50984d1
zhixin <wenzhixin2010@gmail.com>
2015-01-30 11:28:51 +0800

    Update issue template.

---
+++ template.js
@@ -0,0 +1,18 @@
+var fs = require('fs'),
+    sprintf = require('sprintf');
+
+if (process.argv.length < 2) {
+    console.log('You need to enter title');
+    return;
+}
+
+var content = fs.readFileSync('./template.html').toString(),
+    issue = process.argv[2],
+    title = process.argv[3] || '';
+
+content = sprintf(content, {
+    issue: issue,
+    title: title
+});
+
+fs.writeFileSync(sprintf('./%s.html', issue), content);

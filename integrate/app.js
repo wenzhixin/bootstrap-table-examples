8e82493094434fe92e4aabe282df0218e8bdacbb
Dan Kaplun <dbkaplun@gmail.com>
2015-07-05 00:13:53 -0400

    Fix angularjs example

--- integrate/app.js
+++ integrate/app.js
@@ -26,64 +26,66 @@ angular.module('app', ['bsTable'])
         var colData = {workspace: wk.name};
         wk.rows = makeRandomRows(colData);
         setInterval(function () {
-            wk.rows = wk.tableOptions.data = makeRandomRows(colData);
+            wk.rows = wk.bsTableControl.options.data = makeRandomRows(colData);
             $scope.$applyAsync();
         }, 5000);
 
-        wk.tableOptions = {
-            data: wk.rows,
-            rowStyle: function (row, index) {
-                return { classes: 'none' };
-            },
-            cache: false,
-            height: 400,
-            striped: true,
-            pagination: true,
-            pageSize: 10,
-            pageList: [5, 10, 25, 50, 100, 200],
-            search: true,
-            showColumns: true,
-            showRefresh: false,
-            minimumCountColumns: 2,
-            clickToSelect: false,
-            showToggle: true,
-            maintainSelected: true,
-            columns: [{
-                field: 'state',
-                checkbox: true
-            }, {
-                field: 'index',
-                title: '#',
-                align: 'right',
-                valign: 'bottom',
-                sortable: true
-            }, {
-                field: 'id',
-                title: 'Item ID',
-                align: 'center',
-                valign: 'bottom',
-                sortable: true
-            }, {
-                field: 'name',
-                title: 'Item Name',
-                align: 'center',
-                valign: 'middle',
-                sortable: true
-            }, {
-                field: 'workspace',
-                title: 'Workspace',
-                align: 'left',
-                valign: 'top',
-                sortable: true
-            }, {
-                field: 'flag',
-                title: 'Flag',
-                align: 'center',
-                valign: 'middle',
+        wk.bsTableControl = {
+            options: {
+                data: wk.rows,
+                rowStyle: function (row, index) {
+                    return { classes: 'none' };
+                },
+                cache: false,
+                height: 400,
+                striped: true,
+                pagination: true,
+                pageSize: 10,
+                pageList: [5, 10, 25, 50, 100, 200],
+                search: true,
+                showColumns: true,
+                showRefresh: false,
+                minimumCountColumns: 2,
                 clickToSelect: false,
-                formatter: flagFormatter,
-                // events: flagEvents
-            }]
+                showToggle: true,
+                maintainSelected: true,
+                columns: [{
+                    field: 'state',
+                    checkbox: true
+                }, {
+                    field: 'index',
+                    title: '#',
+                    align: 'right',
+                    valign: 'bottom',
+                    sortable: true
+                }, {
+                    field: 'id',
+                    title: 'Item ID',
+                    align: 'center',
+                    valign: 'bottom',
+                    sortable: true
+                }, {
+                    field: 'name',
+                    title: 'Item Name',
+                    align: 'center',
+                    valign: 'middle',
+                    sortable: true
+                }, {
+                    field: 'workspace',
+                    title: 'Workspace',
+                    align: 'left',
+                    valign: 'top',
+                    sortable: true
+                }, {
+                    field: 'flag',
+                    title: 'Flag',
+                    align: 'center',
+                    valign: 'middle',
+                    clickToSelect: false,
+                    formatter: flagFormatter,
+                    // events: flagEvents
+                }]
+            }
         };
         function flagFormatter(value, row, index) {
             return '<img src="' + row.flagImage + '"/>'

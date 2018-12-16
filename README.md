4f4b6bf1b17ebb67cdc0a0ccf1cc4548eb04eb58
zhixin <wenzhixin2010@gmail.com>
2018-12-16 16:13:47 +0800

    Finish issues example

--- README.md
+++ README.md
@@ -7,35 +7,6 @@ bootstrap-table-examples
 
 [CRUD Example](https://github.com/wenzhixin/bootstrap-table-examples/blob/master/crud/README.md)
 
-## Usage
-
-### clone & init
-
-```sh
-git clone https://github.com/wenzhixin/bootstrap-table-examples.git
-cd bootstrap-table-examples
-git submodule update --init
-
-npm install
-```
-
-### download from zip
-
-* Download [bootstrap-table-examples](https://github.com/wenzhixin/bootstrap-table-examples/archive/master.zip)
-* Download [bootstrap-table](https://github.com/wenzhixin/bootstrap-table/archive/master.zip), unzip to `assets/bootstrap-table`
-* Download [multiple-select](https://github.com/wenzhixin/multiple-select/archive/master.zip), unzip to `assets/multiple-select`
-
-```
-cd bootstrap-table-examples
-npm install
-```
-
-## run
-
-```sh
-node app
-```
-
 ## reporting issues
 
 All issues need to be submitted to the main project, not this examples repo.
--- integrate/angular/app.js
+++ integrate/angular/app.js
@@ -1,104 +1,105 @@
-//http://wenzhixin.net.cn/p/bootstrap-table/docs/getting-started.html
-angular.module('app', ['bsTable'])
-.controller('MainController', function ($scope, $http) {
-    $scope.workspaces = [];
-    $scope.workspaces.push({ name: 'Workspace 1' });
-    $scope.workspaces.push({ name: 'Workspace 2' });
-    $scope.workspaces.push({ name: 'Workspace 3' });
+// http://wenzhixin.net.cn/p/bootstrap-table/docs/getting-started.html
+window.angular.module('app', ['bsTable'])
+  .controller('MainController', function ($scope, $http) {
+    $scope.workspaces = []
+    $scope.workspaces.push({ name: 'Workspace 1' })
+    $scope.workspaces.push({ name: 'Workspace 2' })
+    $scope.workspaces.push({ name: 'Workspace 3' })
 
     function makeRandomRows (colData) {
-        var rows = [];
-        for (var i = 0; i < 500; i++) {
-            rows.push($.extend({
-                index: i,
-                id: 'row ' + i,
-                name: 'GOOG' + i,
-                flagImage: Math.random() < 0.4
-                    ? 'img/blueFlag16.png'
-                    : Math.random() < 0.75
-                        ? 'img/yellowFlag16.png'
-                        : 'img/greenFlag16.png'
-            }, colData));
-        }
-        return rows;
+      var rows = []
+      for (var i = 0; i < 500; i++) {
+        rows.push($.extend({
+          index: i,
+          id: 'row ' + i,
+          name: 'GOOG' + i,
+          flagImage: Math.random() < 0.4
+            ? 'img/blueFlag16.png'
+            : Math.random() < 0.75
+              ? 'img/yellowFlag16.png'
+              : 'img/greenFlag16.png'
+        }, colData))
+      }
+      return rows
     }
+
+    function flagFormatter(value, row, index) {
+      return '<img src="' + row.flagImage + '"/>'
+    }
+
     $scope.workspaces.forEach(function (wk,index) {
-        var colData = {workspace: wk.name};
-        wk.rows = makeRandomRows(colData);
+      var colData = {workspace: wk.name}
+      wk.rows = makeRandomRows(colData)
 
-        wk.bsTableControl = {
-            options: {
-                data: wk.rows,
-                rowStyle: function (row, index) {
-                    return { classes: 'none' };
-                },
-                cache: false,
-                height: 400,
-                striped: true,
-                pagination: true,
-                pageSize: 10,
-                pageList: [5, 10, 25, 50, 100, 200],
-                search: true,
-                showColumns: true,
-                showRefresh: false,
-                minimumCountColumns: 2,
-                clickToSelect: false,
-                showToggle: true,
-                maintainSelected: true,
-                columns: [{
-                    field: 'state',
-                    checkbox: true
-                }, {
-                    field: 'index',
-                    title: '#',
-                    align: 'right',
-                    valign: 'bottom',
-                    sortable: true
-                }, {
-                    field: 'id',
-                    title: 'Item ID',
-                    align: 'center',
-                    valign: 'bottom',
-                    sortable: true
-                }, {
-                    field: 'name',
-                    title: 'Item Name',
-                    align: 'center',
-                    valign: 'middle',
-                    sortable: true
-                }, {
-                    field: 'workspace',
-                    title: 'Workspace',
-                    align: 'left',
-                    valign: 'top',
-                    sortable: true
-                }, {
-                    field: 'flag',
-                    title: 'Flag',
-                    align: 'center',
-                    valign: 'middle',
-                    clickToSelect: false,
-                    formatter: flagFormatter,
-                    // events: flagEvents
-                }]
-            }
-        };
-        function flagFormatter(value, row, index) {
-            return '<img src="' + row.flagImage + '"/>'
+      wk.bsTableControl = {
+        options: {
+          data: wk.rows,
+          rowStyle: function (row, index) {
+            return { classes: 'none' }
+          },
+          cache: false,
+          height: 400,
+          striped: true,
+          pagination: true,
+          pageSize: 10,
+          pageList: [5, 10, 25, 50, 100, 200],
+          search: true,
+          showColumns: true,
+          showRefresh: false,
+          minimumCountColumns: 2,
+          clickToSelect: false,
+          showToggle: true,
+          maintainSelected: true,
+          columns: [{
+            field: 'state',
+            checkbox: true
+          }, {
+            field: 'index',
+            title: '#',
+            align: 'right',
+            valign: 'bottom',
+            sortable: true
+          }, {
+            field: 'id',
+            title: 'Item ID',
+            align: 'center',
+            valign: 'bottom',
+            sortable: true
+          }, {
+            field: 'name',
+            title: 'Item Name',
+            align: 'center',
+            valign: 'middle',
+            sortable: true
+          }, {
+            field: 'workspace',
+            title: 'Workspace',
+            align: 'left',
+            valign: 'top',
+            sortable: true
+          }, {
+            field: 'flag',
+            title: 'Flag',
+            align: 'center',
+            valign: 'middle',
+            clickToSelect: false,
+            formatter: flagFormatter
+            // events: flagEvents
+          }]
         }
-
-    });
+      }
+    })
 
 
     $scope.changeCurrentWorkspace = function (wk) {
-        $scope.currentWorkspace = wk;
-    };
+      $scope.currentWorkspace = wk
+    }
 
 
-    //Select the workspace in document ready event
+    // Select the workspace in document ready event
     $(document).ready(function () {
-        $scope.changeCurrentWorkspace($scope.workspaces[0]);
-        $scope.$apply();
-    });
+      $scope.changeCurrentWorkspace($scope.workspaces[0])
+      $scope.$apply()
+    })
 
-});
+  })

51a841763c37148ad19943740e418de004c27864
zhixin <wenzhixin2010@gmail.com>
2018-12-17 08:23:18 +0800

    Fix ad style and support mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ function loadUrl(url) {
   if (location.search.slice(1) === 'view-source') {
     viewSource = '#view-source'
   }
-  $('iframe').attr('src', template + '?v=1&' + url + viewSource)
+  $('iframe').attr('src', template + '?v=2&' + url + viewSource)
 }
 
 function initNavigation(href) {

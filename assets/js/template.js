8e57f8de756f6e8d48b865bde4f191bec7ffbe76
Travis CI <wenzhixin2010@gmail.com>
2018-12-27 14:27:48 +0000

    Deploy to GitHub Pages: 9c7c90d4c7e95d07bf3427919c34c2ad1451af3c

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,9 +1,9 @@
 $(function () {
-  var url = location.search.replace(/\?v=\d+&/, '').replace(/\?v=5&/, '')
+  var url = location.search.replace(/\?v=\d+&/, '').replace(/\?v=6&/, '')
   var isSource = location.hash.slice(1) === 'view-source'
   $.ajax({
     type: 'GET',
-    url: url + '?v=5', // todo: add version to solve cache problem
+    url: url + '?v=6', // todo: add version to solve cache problem
     dataType: 'html',
     global: false,
     cache: true, // (warning: setting it to false will cause a timestamp and will call the request twice)

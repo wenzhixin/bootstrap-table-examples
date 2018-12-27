f31c0edf62c0066fdbb11c2dc1589d18ae00fb71
zhixin <wenzhixin2010@gmail.com>
2018-12-27 23:17:07 +0800

    Export add showToggle

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,7 @@ function doCompile {
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
+    echo "Skipping deploy."
     exit 0
 fi
 

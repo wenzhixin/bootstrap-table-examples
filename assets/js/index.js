f41c9e7ca5fb3c3b9f761a8322c7c11990a91dc5
Travis CI <wenzhixin2010@gmail.com>
2019-02-02 15:42:46 +0000

    Deploy to GitHub Pages: 418d1f42a17a0e6c40f5716755794d56bc3809df

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ function loadUrl(url_) {
   } else if (location.search.slice(1) === 'is-debug') {
     hash = '#is-debug'
   }
-  var url = template + '?v=36&url=' + url_ + hash
+  var url = template + '?v=37&url=' + url_ + hash
   if (window._config.isDebug) {
     url = template + '?t=' + (+new Date()) + '&url=' + url_ + hash
   }
@@ -28,6 +28,64 @@ function initNavigation(href) {
   $('#bd-docs-nav .active').removeClass('active')
   $el.parent().addClass('active')
   $el.parents('.bd-toc-item').addClass('active')
+}
+
+function doSearch() {
+  var searchClient = window.algoliasearch('FXDJ517Z8G', '9b89c4a7048370f4809b0bc77b2564ac');
+
+  var search = window.instantsearch({
+    indexName: 'bootstrap-table-example',
+    searchClient: searchClient,
+    searchFunction: function (helper) {
+      if (helper.state.query) {
+        helper.search()
+        $('#hits').show()
+      } else {
+        $('#hits').hide()
+      }
+    }
+  })
+
+  search.addWidget(
+    window.instantsearch.widgets.searchBox({
+      container: '#searchbox'
+    })
+  )
+
+  search.addWidget(
+    window.instantsearch.widgets.hits({
+      container: '.hits-body',
+      templates: {
+        item: function (hit) {
+          return [
+            '<div class="link" data-href="' + hit.url + hit.anchor + '">',
+            '<div class="category">',
+            hit.anchor.split('/')[0].slice(1),
+            '</div>',
+            '<div class="title">',
+            hit.title,
+            '</div>',
+            '<div class="description">',
+            hit.description,
+            '</div>',
+            '</div>'
+          ].join('')
+        }
+      }
+    })
+  )
+
+  $(document).on('click', '.ais-Hits-item .link', function (e) {
+    var href = $(e.currentTarget).data('href')
+    if ($(e.target).is('a')) {
+      return
+    }
+    location.href = href
+    $('.ais-SearchBox-reset').click()
+    $('.ais-SearchBox-input').blur()
+  })
+
+  search.start()
 }
 
 $(function () {
@@ -68,4 +126,5 @@ $(function () {
   var href = location.hash.substring(1) || 'welcome.html'
   loadUrl(href)
   initNavigation(href)
+  doSearch()
 })

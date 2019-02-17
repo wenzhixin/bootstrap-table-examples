9e4a47266be69ae47686bb35174e2be577623ce9
Travis CI <wenzhixin2010@gmail.com>
2019-02-17 01:10:23 +0000

    Deploy to GitHub Pages: 6aee410466d6731c30ef9ae1bc1dfb9ee616ce6b

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,26 +7,35 @@ function loadUrl(url_) {
   if (location.search.slice(1) === 'is-debug') {
     hash = '#is-debug'
   }
-  var url = 'template.html?v=48&url=' + url_ + hash
+  var url = 'template.html?v=49&url=' + url_ + hash
   if (window._config.isDebug) {
     url = 'template.html?t=' + (+new Date()) + '&url=' + url_ + hash
   }
   if (url_.indexOf('view-source') > -1) {
-    url = 'template.html?v=48&view-source&url=' + url_ + hash
+    url = 'template.html?v=49&view-source&url=' + url_ + hash
   }
   $('iframe').attr('src', url)
 }
 
 function initNavigation(href) {
   var $el = $('a[href="#' + href + '"]')
+  var $parent = $el.parent()
 
   if (!$el.length) {
     return
   }
 
   $('#bd-docs-nav .active').removeClass('active')
-  $el.parent().addClass('active')
+  $parent.addClass('active')
   $el.parents('.bd-toc-item').addClass('active')
+}
+
+function autoScrollNavigation () {
+  var $el = $('.bd-sidenav >li.active')
+  $('#bd-docs-nav').scrollTop(0)
+  if ($el.offset().top > $(window).height() / 2) {
+    $('#bd-docs-nav').scrollTop($el.offset().top - $(window).height() / 2)
+  }
 }
 
 function doSearch() {
@@ -82,39 +91,18 @@ function doSearch() {
     location.href = href
     $('.ais-SearchBox-reset').click()
     $('.ais-SearchBox-input').blur()
+    setTimeout(autoScrollNavigation, 200)
   })
 
   search.start()
 }
 
 $(function () {
-  $('.toggle').click(function () {
-    $('.nav-list').toggleClass('active')
-  })
-
   $('.bd-sidenav li').each(function () {
     $(this).attr('title', $.trim($(this).text()))
   })
 
   $('[data-toggle="tooltip"]').tooltip()
-
-  $(document).on('click', '#navbar li a, .nav-list li a, .navigation a', function (e) {
-    var href = $(this).attr('href')
-    if (href === '#' || /^http.*/.test(href)) {
-      return
-    }
-    e.preventDefault()
-    if (location.search.slice(1) === 'view-source') {
-      location.href = 'index.html#' + href
-      return
-    }
-
-    $(this).blur()
-    $('.nav-list').removeClass('active')
-    location.hash = href
-    loadUrl(href)
-    initNavigation(href)
-  })
 
   $(window).hashchange(function () {
     var href = location.hash.substring(1)
@@ -122,12 +110,9 @@ $(function () {
     initNavigation(href)
   })
 
-  $(window).on('blur',function() {
-    $('.dropdown-toggle').parent().removeClass('open')
-  })
-
   var href = location.hash.substring(1) || 'welcome.html'
   loadUrl(href)
   initNavigation(href)
+  autoScrollNavigation()
   doSearch()
 })

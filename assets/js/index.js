7794869c16688d64186208ebaa99f4136cde43b6
Travis CI <wenzhixin2010@gmail.com>
2019-04-16 00:44:13 +0000

    Deploy to GitHub Pages: 7b9480b0897be5b24b7ea3fbe2674d0fcdb48f5d

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,12 +37,12 @@ function loadUrl(url_) {
   if (window._config.themes.indexOf(window._config.theme) > -1) {
     template += '-' + window._config.theme
   }
-  var url = template + '.html?v=76&url=' + url_
+  var url = template + '.html?v=77&url=' + url_
   if (window._config.isDebug) {
     url = template + '.html?t=' + (+new Date()) + '&url=' + url_
   }
   if (window._config.isViewSource) {
-    url = template + '.html?v=76&view-source&url=' + url_ + '#view-source'
+    url = template + '.html?v=77&view-source&url=' + url_ + '#view-source'
   }
   $('iframe').attr('src', url)
 }

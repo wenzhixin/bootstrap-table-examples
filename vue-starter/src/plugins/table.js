7609d4ef7254faf48b1264c74bccbf59e3589b52
zhixin <wenzhixin2010@gmail.com>
2019-06-25 00:17:45 +0800

    Add vue-starter

---
+++ vue-starter/src/plugins/table.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-table/dist/bootstrap-table.min.css'
+
+import './jquery.js'
+import Vue from 'vue'
+import 'bootstrap'
+import 'bootstrap-table/dist/bootstrap-table'
+import BootstrapTable from 'bootstrap-table/dist/bootstrap-table-vue.esm.js'
+
+Vue.component('BootstrapTable', BootstrapTable)

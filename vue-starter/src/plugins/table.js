f4208ca7f422f3885b1304a067559fc163c71201
zhixin <wenzhixin2010@gmail.com>
2019-06-25 00:20:41 +0800

    Update js

--- vue-starter/src/plugins/table.js
+++ vue-starter/src/plugins/table.js
@@ -4,7 +4,7 @@ import 'bootstrap-table/dist/bootstrap-table.min.css'
 import './jquery.js'
 import Vue from 'vue'
 import 'bootstrap'
-import 'bootstrap-table/dist/bootstrap-table'
+import 'bootstrap-table/dist/bootstrap-table.js'
 import BootstrapTable from 'bootstrap-table/dist/bootstrap-table-vue.esm.js'
 
 Vue.component('BootstrapTable', BootstrapTable)

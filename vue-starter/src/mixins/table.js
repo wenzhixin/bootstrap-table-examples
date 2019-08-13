770d35eaee01d2ffb44daa4ae3a2c6e39972238c
zhixin <wenzhixin2010@gmail.com>
2019-08-13 23:58:24 +0800

    Update vue alias

--- vue-starter/src/mixins/table.js
+++ vue-starter/src/mixins/table.js
@@ -1,4 +1,4 @@
-import Vue from 'vue/dist/vue.esm.js'
+import Vue from 'vue'
 
 export default {
   data () {
--- vue-starter/src/plugins/table.js
+++ vue-starter/src/plugins/table.js
@@ -3,7 +3,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap-table/dist/bootstrap-table.min.css'
 
 import './jquery.js'
-import Vue from 'vue/dist/vue.esm.js'
+import Vue from 'vue'
 import 'bootstrap'
 import 'bootstrap-table/dist/bootstrap-table.js'
 import BootstrapTable from 'bootstrap-table/dist/bootstrap-table-vue.esm.js'

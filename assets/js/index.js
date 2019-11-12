dcae68c9f2dfeb57a3befb7fd258d7a63fb5368a
Travis CI <wenzhixin2010@gmail.com>
2019-11-12 01:44:05 +0000

    Deploy to GitHub Pages: efcca178d0713d48a47a1a47312981f1ec03dfa7

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,12 +37,12 @@ function loadUrl(url_) {
   if (window._config.themes.indexOf(window._config.theme) > -1) {
     template += '-' + window._config.theme
   }
-  var url = template + '.html?v=112&url=' + url_
+  var url = template + '.html?v=113&url=' + url_
   if (window._config.isDebug) {
     url = template + '.html?t=' + (+new Date()) + '&url=' + url_
   }
   if (window._config.isViewSource) {
-    url = template + '.html?v=112&view-source&url=' + url_ + '#view-source'
+    url = template + '.html?v=113&view-source&url=' + url_ + '#view-source'
   }
   $('iframe').attr('src', url)
 }

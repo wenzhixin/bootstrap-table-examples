5f8bb00b7293f248c235a0dab8f3eadd94120348
zhixin <wenzhixin2010@gmail.com>
2024-02-27 14:20:17 +0800

    Build 1.22.3

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,7 +1,7 @@
 window._config = {
   isDebug: location.hash.slice(1) === 'is-debug' ||
     ['localhost', '127.0.0.1', 'dev.bootstrap-table.com'].indexOf(location.hostname) > -1,
-  cdnUrl: 'https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.2/dist/',
+  cdnUrl: 'https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.3/dist/',
   localUrl: '../bootstrap-table/src/',
   testUrl: '/src/'
 }
